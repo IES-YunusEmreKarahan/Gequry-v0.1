5b04e280ac07793a88b6789d159c083ea54ca12d
IES-YunusEmreKarahan <147061126+IES-YunusEmreKarahan@users.noreply.github.com>
2024-12-24 21:11:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const http = require('http');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const { Server } = require('socket.io');
 const bodyParser = require('body-parser');
 
-const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 
 // Middleware
 app.use(bodyParser.json());
